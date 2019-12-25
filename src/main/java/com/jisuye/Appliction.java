fd4056042983205540a99ef49ebbf90a56e8bd92
ixx <cxmm@vip.qq.com>
2019-12-25 18:35:20 +0800

    init

---
+++ src/main/java/com/jisuye/Appliction.java
@@ -0,0 +1,12 @@
+package com.jisuye;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Appliction {
+    public static void main(String[] args) {
+        run(Appliction.class, args);
+    }
+}
